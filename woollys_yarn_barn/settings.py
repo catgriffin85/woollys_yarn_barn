7c38cfcbf50c3b27a3648f37fb405040a736130d
catgriffin85 <catherine.griffin@students.codeinstitute.net>
2025-03-31 21:52:46 +0100

    Create checkout.html with checkout.css and install crispy forms

--- woollys_yarn_barn/settings.py
+++ woollys_yarn_barn/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'stock',
     'cart',
     'checkout',
+
+    # Others
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'woollys_yarn_barn.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
--- woollys_yarn_barn/urls.py
+++ woollys_yarn_barn/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('stock/', include('stock.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
